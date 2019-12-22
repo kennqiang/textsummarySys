6a189c36ac408ec06877f23e695787c911593470
kennqiang <kennqiang@outlook.com>
2019-12-22 19:34:40 +0800

    添加seq2seq部分

---
+++ myapp/seq2seq.py
@@ -0,0 +1,7 @@
+class seq2seq:
+    def __init__(self,weight_path=None):
+        pass
+    def predict(self,content):
+        return "seq2seq summary"
+    def pre_process(self,content):
+        pass
